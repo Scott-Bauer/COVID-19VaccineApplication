53cecec8640d2a75f659b32c6d6cca0086bba91a
Scott Bauer <59296781+scottyB1234@users.noreply.github.com>
2021-03-15 18:01:31 -0400

    Update loaddata.sql

--- SQL/loaddata.sql
+++ SQL/loaddata.sql
@@ -1,6 +1,6 @@
 
-
-LOAD DATA INFILE "C:\Users\scott\Desktop\p2data\category.csv" INTO TABLE category
+--EXAMPLE OF HOW TO LOAD DATA GIVEN CSV--
+LOAD DATA INFILE "~\category.csv" INTO TABLE category
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
